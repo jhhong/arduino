eea48c0d4011a4218f747cbceb032a53a0ac43fe
JeongHo Hong <visioner7@hanmail.net>
2015-10-28 02:42:08 +0900

    LCD example

---
+++ lcdi2c/lcdi2c.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>  // Comes with Arduino IDE
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // or 0x27
+
+void setup()
+{
+  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
+  lcd.backlight(); // finish with backlight on  
+  lcd.clear();
+}
+
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  lprint("Hello!","Arduino in Life!",2000);
+  lprint("I'm leader Isaac","This is LCD Test",3000);
+  lprint("Turn off light","wait minutes...",3000);  
+  lcd.noBacklight();
+  delay(2000);
+  lcd.backlight();  
+  lprint("Turn on light","welcome back",3000);    
+}
+
+void lprint(char *str1, char*str2,int waitTime) {
+  lcd.clear();
+  lcd.setCursor(0,0);  
+  lcd.print(str1);
+  lcd.setCursor(0,1);  
+  lcd.print(str2);
+  delay(waitTime);
+}
