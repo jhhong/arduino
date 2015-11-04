df0c7c01a7dfd858bd93ac865efaa1cb884c9794
JeongHo Hong <visioner7@hanmail.net>
2015-11-05 01:50:46 +0900

    pir with sleep

---
+++ pir2/pir2.ino
@@ -0,0 +1,70 @@
+#include <avr/sleep.h>
+#include <SoftwareSerial.h>
+
+int ledPin1 = 8;       // ledPin을 13번에 추가
+int ledPin2 = 9;       // ledPin을 13번에 추가
+int ledPin3 = 10;       // ledPin을 13번에 추가
+int pirPin = 2;     // inputPin을 2번에 추가
+long startTime = 0;
+bool needLedOn = false;
+
+SoftwareSerial dbgSerial(0, 1); // RX, TX
+  
+void setup() {
+  pinMode(pirPin, INPUT);    //inputPin을 입력모드로
+  pinMode(ledPin1, OUTPUT);    //ledPin을 출력모드로
+  pinMode(ledPin2, OUTPUT);    //ledPin을 출력모드로
+  pinMode(ledPin3, OUTPUT);    //ledPin을 출력모드로  
+  dbgSerial.begin(9600);           //시리얼 통신을, 9600속도로 받습니다. ( 숫자 조정은 자유)
+}
+
+void loop(){
+
+  ledOn();
+  if (needLedOn) {
+    ledOn();    
+    delay(10000);    
+    needLedOn = false;
+  } else {
+    ledOff();    
+    sleepNow();
+  }
+} 
+
+void ledOn(){
+  digitalWrite(ledPin1,HIGH);
+  digitalWrite(ledPin2,HIGH);
+  digitalWrite(ledPin3,HIGH);  
+}
+
+void ledOff(){
+  digitalWrite(ledPin1,LOW);
+  digitalWrite(ledPin2,LOW);
+  digitalWrite(ledPin3,LOW);  
+}
+
+void wakeUpNow()        // here the interrupt is handled after wakeup
+{
+    needLedOn = true;
+    dbgSerial.println("wake up");
+}
+
+void sleepNow()         // here we put the arduino to sleep
+{
+    dbgSerial.println("Entering sleep mode");  
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+    sleep_enable();          // enables the sleep bit in the mcucr register
+                             // so sleep is possible. just a safety pin 
+
+    attachInterrupt(0,wakeUpNow, HIGH); // use interrupt 0 (pin 2) and run function
+                                       // wakeUpNow when pin 2 gets LOW 
+
+    sleep_mode();            // here the device is actually put to sleep!!
+                             // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+
+    sleep_disable();         // first thing after waking from sleep:
+                             // disable sleep...
+    detachInterrupt(0);      // disables interrupt 0 on pin 2 so the 
+                             // wakeUpNow code will not be executed 
+                             // during normal running time.
+}
