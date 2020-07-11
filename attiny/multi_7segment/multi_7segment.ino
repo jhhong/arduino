b9e7a0582eb34ba15cb62c4e193d8a14be8f1935
jhhong <jhhong@navercorp.com>
2020-07-12 00:00:24 +0900

    각 led 별로 제어 성공.

--- attiny/multi_7segment/multi_7segment.ino
+++ attiny/multi_7segment/multi_7segment.ino
@@ -8,15 +8,13 @@ const int switchPin = PB3; // switch
 int counter = 0;
 
 byte dec_digits[] = {
-  0b11001111,  //1
-  0b10010010,  //2
-  0b10000110,  //3
-  0b11001100,  //4
-  0b10100100,  //5
-  0b10100000,  //6
-  0b10001101,  //7
-  0b11111111,  //8
-  0b11110011   //9
+  0b10111111,  //1
+  0b11011111,  //2
+  0b11101111,  //3
+  0b11110111,  //4
+  0b11111011,  //5
+  0b11111101,  //6
+  0b11111110  //7
 };
 
 void setup() {
@@ -28,15 +26,16 @@ void setup() {
 
   //need ?
   GIMSK = 0b00100000;
-  PCMSK = 0b00001000;  
+  PCMSK = 0b00001000;
 }
 
 void loop() {
 
   //  int count = sizeof(dec_digits) / sizeof(dec_digits[0]);
 
-  sleep();
-  waitInput();
+  //  sleep();
+  //  waitInput();
+  showNumber();
 }
 
 void waitInput() {
@@ -51,12 +50,12 @@ void waitInput() {
 
 void showNumber() {
 
-  for (int i = 0; i < 9; i++) {
+  for (int i = 0; i < 7; i++) {
     digitalWrite(latchPin, LOW); // shift out the bits  :
-    shiftOut(dataPin, clockPin, MSBFIRST, 0b11000000); //take the latch pin high so the LEDs will light up:
+    shiftOut(dataPin, clockPin, LSBFIRST, dec_digits[i]); //take the latch pin high so the LEDs will light up:
     digitalWrite(latchPin, HIGH); // pause before next value:
     delay(500);
-  }  
+  }
 }
 
 void sleep() {
