97c25c7373f1107e58a295fa4294d3586f1f69f9
jhhong <jhhong@navercorp.com>
2020-07-11 10:17:49 +0900

    startwards

---
+++ attiny/starwars/starwars.ino
@@ -0,0 +1,169 @@
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+
+const int buzzerPin = PB0;
+const int switchPin = PB3;
+int counter = 0;
+
+#include <avr/sleep.h>
+
+void setup() {
+  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);
+
+  GIMSK = 0b00100000;
+  PCMSK = 0b00001000;
+}
+
+void loop() {
+  sleep();
+  waitInput();
+}
+
+void sleep() {
+  ADCSRA &= ~_BV(ADEN);                   // ADC off
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+
+  sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
+  sei();                                  // Enable interrupts
+  sleep_cpu();                            // sleep
+
+  cli();                                  // Disable interrupts
+  sleep_disable();                        // Clear SE bit
+  ADCSRA |= _BV(ADEN);                    // ADC on
+
+  sei();                                  // Enable interrupts
+}
+
+ISR(PCINT0_vect)
+{
+}
+
+void waitInput() {
+  for (int i = 0; i <= 30000; i++) {
+    if (digitalRead(switchPin) == 1) {
+      delayMicroseconds(300);
+    } else {
+      playSound();
+    }
+  }
+}
+
+void playSound() {
+
+  //Play first section
+  firstSection();
+
+  //Play second section
+  secondSection();
+
+  beep(f, 250);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(a, 125);
+  beep(cH, 500);
+  beep(a, 375);
+  beep(cH, 125);
+  beep(eH, 650);
+
+  delay(500);
+
+  //Repeat second section
+  secondSection();
+
+
+  beep(f, 250);
+  beep(gS, 500);
+  beep(f, 375);
+  beep(cH, 125);
+  beep(a, 500);
+  beep(f, 375);
+  beep(cH, 125);
+  beep(a, 650);
+
+  delay(650);
+}
+
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(buzzerPin, note);
+  
+  delay(duration);
+  //Stop tone on buzzerPin
+  noTone(buzzerPin);
+
+  delay(50);
+
+  //Increment counter
+  counter++;
+}
+
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+
+  delay(500);
+
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+
+  delay(500);
+}
+
+void secondSection()
+{
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);
+  beep(fSH, 250);
+
+  delay(325);
+
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);
+  beep(cSH, 175);
+  beep(cH, 125);
+  beep(b, 125);
+  beep(cH, 250);
+
+  delay(350);
+}
