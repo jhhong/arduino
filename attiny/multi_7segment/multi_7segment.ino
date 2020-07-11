9fa85a468039771b8ee104c09413e4850182b783
jhhong <jhhong@navercorp.com>
2020-07-11 16:04:55 +0900

    버튼 입력 연동

--- attiny/multi_7segment/multi_7segment.ino
+++ attiny/multi_7segment/multi_7segment.ino
@@ -1,6 +1,11 @@
-int dataPin = PB0;   // 74HC595의 data(DS) 핀을 연결
-int latchPin = PB1;  // 74HC595의 latch(ST_CP) 핀을 연결
-int clockPin = PB2;  // 74HC595의 clock(SH_CP) 핀을 연결
+#include <avr/sleep.h>#include <avr/sleep.h>
+
+const int dataPin = PB0;   // 74HC595의 data(DS) 핀을 연결
+const int latchPin = PB1;  // 74HC595의 latch(ST_CP) 핀을 연결
+const int clockPin = PB2;  // 74HC595의 clock(SH_CP) 핀을 연결
+const int switchPin = PB3; // switch
+
+int counter = 0;
 
 byte dec_digits[] = {
   0b11001111,  //1
@@ -19,16 +24,56 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP);
+
+  //need ?
+  GIMSK = 0b00100000;
+  PCMSK = 0b00001000;  
 }
 
 void loop() {
 
   //  int count = sizeof(dec_digits) / sizeof(dec_digits[0]);
 
+  sleep();
+  waitInput();
+}
+
+void waitInput() {
+  for (int i = 0; i <= 30000; i++) {
+    if (digitalRead(switchPin) == 1) {
+      delayMicroseconds(300);
+    } else {
+      showNumber();
+    }
+  }
+}
+
+void showNumber() {
+
   for (int i = 0; i < 9; i++) {
     digitalWrite(latchPin, LOW); // shift out the bits  :
     shiftOut(dataPin, clockPin, MSBFIRST, 0b11000000); //take the latch pin high so the LEDs will light up:
     digitalWrite(latchPin, HIGH); // pause before next value:
     delay(500);
-  }
+  }  
+}
+
+void sleep() {
+  ADCSRA &= ~_BV(ADEN);                   // ADC off
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
+
+  sleep_enable();                         // Sets the Sleep Enable bit in the MCUCR Register (SE BIT)
+  sei();                                  // Enable interrupts
+  sleep_cpu();                            // sleep
+
+  cli();                                  // Disable interrupts
+  sleep_disable();                        // Clear SE bit
+  ADCSRA |= _BV(ADEN);                    // ADC on
+
+  sei();                                  // Enable interrupts
+}
+
+ISR(PCINT0_vect)
+{
 }
