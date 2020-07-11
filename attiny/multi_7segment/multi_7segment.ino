049b3289d08c177fb3388371a15ac4669bc944f2
jhhong <jhhong@navercorp.com>
2020-07-11 15:56:53 +0900

    gnd 신호 변경.

--- attiny/multi_7segment/multi_7segment.ino
+++ attiny/multi_7segment/multi_7segment.ino
@@ -3,15 +3,14 @@ int latchPin = PB1;  // 74HC595의 latch(ST_CP) 핀을 연결
 int clockPin = PB2;  // 74HC595의 clock(SH_CP) 핀을 연결
 
 byte dec_digits[] = {
-  0b10000000,
-  0b10000000,
-  0b11000000,
-  0b10100000,
-  0b10010000,
-  0b10001000,
-  0b10000100,
-  0b10000010,
-  0b10000001
+  0b11001111,  //1
+  0b10010010,  //2
+  0b10000110,  //3
+  0b11001100,  //4
+  0b10100100,  //5
+  0b10100000,  //6
+  0b10001101,  //7
+  0b11111111   //8
 };
 
 void setup() {
@@ -23,11 +22,11 @@ void setup() {
 
 void loop() {
 
-  int count = sizeof(dec_digits)/sizeof(dec_digits[0]);
-  
-  for (int i = 0; i < 9; i++) {
+  //  int count = sizeof(dec_digits) / sizeof(dec_digits[0]);
+
+  for (int i = 0; i < 7; i++) {
     digitalWrite(latchPin, LOW); // shift out the bits  :
-    shiftOut(dataPin, clockPin, MSBFIRST, i); //take the latch pin high so the LEDs will light up:
+    shiftOut(dataPin, clockPin, MSBFIRST, 0b11000000); //take the latch pin high so the LEDs will light up:
     digitalWrite(latchPin, HIGH); // pause before next value:
     delay(500);
   }
