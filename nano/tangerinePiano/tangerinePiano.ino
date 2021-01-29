b0acaa1945cf0609e9330c64d7b7b5f229597648
jhhong <visioner7@naver.com>
2021-01-30 00:14:28 +0900

    mpr121

---
+++ nano/tangerinePiano/tangerinePiano.ino
@@ -0,0 +1,141 @@
+/*********************************************************
+This is a library for the MPR121 12-channel Capacitive touch sensor
+
+Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
+  ----> https://www.adafruit.com/products/
+
+These sensors use I2C communicate, at least 2 pins are required 
+to interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada for Adafruit Industries.  
+BSD license, all text above must be included in any redistribution
+**********************************************************/
+
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+
+#ifndef _BV
+#define _BV(bit) (1 << (bit)) 
+#endif
+
+#define T_C 262
+#define T_D 294
+#define T_E 330
+#define T_F 349
+#define T_G 392
+#define T_A 440
+#define T_B 493
+#define T_CC 523
+
+const int speaker_pwr = 2;
+const int speaker = 3;
+
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(speaker_pwr,OUTPUT);
+  pinMode(speaker,OUTPUT);
+  
+  while (!Serial) { // needed to keep leonardo/micro from starting too fast!
+    delay(10);
+  }
+  
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
+  
+  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // If tied to SDA its 0x5C and if SCL then 0x5D
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+}
+
+void loop() {
+  // Get the currently touched pads
+
+  digitalWrite(speaker_pwr, HIGH);
+  
+  currtouched = cap.touched();
+  
+  for (uint8_t i=0; i<12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); 
+      Serial.println(" touched");
+
+      if(i == 0) {
+        tone(speaker,T_C);  
+      }
+
+      if(i==1) {
+        tone(speaker,T_D);
+      }
+
+      if(i==2) {
+        tone(speaker,T_E);
+      }
+
+      if(i==3) {
+        tone(speaker,T_F);
+      }
+
+      if(i==4) {
+        tone(speaker,T_G);
+      }
+
+      if(i==5) {
+        tone(speaker,T_A);
+      }
+
+      if(i==6) {
+        tone(speaker,T_B);
+      }
+
+      if(i==7) {
+        tone(speaker,T_CC);
+      }
+            
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); 
+      Serial.println(" released");
+      noTone(speaker);
+    }
+  }
+
+  // reset our state
+  lasttouched = currtouched;
+
+  // comment out this line for detailed data from the sensor!
+  return;
+  
+  // debugging info, what
+  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
+  Serial.print("Filt: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.filteredData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  Serial.print("Base: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.baselineData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  
+  // put a delay so it isn't overwhelming
+  delay(100);
+}
