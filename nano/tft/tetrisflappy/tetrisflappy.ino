1ae46766e5ae4d6e86dbcfd96c3d6d74dd66060b
isaac <jhhong@navercorp.com>
2023-07-20 21:54:13 +0900

    랜덤 실행하도록 설정.

--- nano/tft/tetrisflappy/tetrisflappy.ino
+++ nano/tft/tetrisflappy/tetrisflappy.ino
@@ -2,16 +2,31 @@
 #include "tetris.h"
 #include "flappy.h"
 
+String games[2] = {"tetris","flappy"};
+String game = games[0];
+
 void setup() {
+  
+  randomSeed(analogRead(0));
+  game = games[random(0,2)];
+
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(COLOR_BLACK);
 
   Serial.begin(9600);
-  setup_tetris();
-  setup_flappy();
+
+  if(game == "tetris") {
+    setup_tetris();
+  } else {
+    setup_flappy();
+  }
 }
 
 void loop() {
-  loop_tetris();
-  loop_flappy();
+
+  if(game == "tetris") {
+    loop_tetris();
+  } else {
+    loop_flappy();
+  }
 }
