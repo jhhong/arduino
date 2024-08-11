9dab49aea13c14e487900ee720d1f94922b4707d
isaac <jhhong@navercorp.com>
2024-08-12 01:35:43 +0900

    주파수 계산하는 방식으로 수정

--- nano/jukebox/jukebox.ino
+++ nano/jukebox/jukebox.ino
@@ -1,26 +1,64 @@
-#define C6  1047
-#define D6  1175
-#define E6  1319
-#define F6  1397
-#define G6  1568
-#define A6  1760
-#define B6  1976
-#define C7  2093
-#define D7  2349
-#define E7  2637
-#define F7  2794
-#define G7  3136
-#define A7  3520
-#define B7  3951
-#define C8  4186
+#define MAX_KEYS 10
+int currentSize = 0;
+
+struct KeyFrequencyMap {
+  char key;
+  long frequency;
+};
+
+KeyFrequencyMap keyFrequencyMap[MAX_KEYS];
+
+void addKeyValue(char key, int value) {
+  if (currentSize < MAX_KEYS) {
+    keyFrequencyMap[currentSize].key = key;
+    keyFrequencyMap[currentSize].frequency = value;
+    currentSize++;
+  }
+}
 
 long WDPI = 1500000;
 int piezo = 8;
 
 void setup() {
+
+  Serial.begin(115200);
+
+  addKeyValue('C', 33); //32.7032
+  addKeyValue('D', 37); //36.7081
+  addKeyValue('E', 41); //41.2034
+  addKeyValue('F', 44); //43.6535
+  addKeyValue('G', 49); //48.9994
+  addKeyValue('A', 55); //55.0000
+  addKeyValue('B', 62); //61.7354
+
   pinMode(piezo, OUTPUT);
 
   HAPPY_BIRTHDAY_TO_YOU();
+}
+
+//C, 4
+long getFrequency(char key, int octave){
+  long frequency = 0;
+
+  for(int i = 0; i < MAX_KEYS; i++) {
+    if(keyFrequencyMap[i].key == key){
+      frequency =  keyFrequencyMap[i].frequency;
+      break;
+    }
+  }
+
+  long octaveFrequency = frequency * pow(2, octave - 1);
+
+  Serial.print("key=");
+  Serial.println(key);
+  Serial.print("frequency=");
+  Serial.println(frequency);  
+  Serial.print("octave=");
+  Serial.println(octave);
+  Serial.print("octaveFrequency=");
+  Serial.println(octaveFrequency);
+
+  return octaveFrequency;
 }
 
 void loop() {
@@ -30,35 +68,62 @@ void loop() {
 
 //happy birthday to u
 void HAPPY_BIRTHDAY_TO_YOU() {
-  buzz(G6, 125);
-  buzz(G6, 125);
-  buzz(A6, 250);
-  buzz(G6, 250);
-  buzz(C7, 250);
-  buzz(B6, 500);
-  buzz(G6, 125);
-  buzz(G6, 125);
-  buzz(A6, 250);
-  buzz(G6, 250);
-  buzz(D7, 250);
-  buzz(C7, 500);
-  buzz(G6, 125);
-  buzz(G6, 125);
-  buzz(G7, 250);
-  buzz(E7, 250);
-  buzz(C7, 250);
-  buzz(B6, 250);
-  buzz(A6, 250);
-  buzz(F7, 125);
-  buzz(F7, 125);
-  buzz(E7, 250);
-  buzz(C7, 250);
-  buzz(D7, 250);
-  buzz(C7, 500);
+buzz('G',6,125);
+buzz('G',6,125);
+buzz('A',6,250);
+buzz('G',6,250);
+buzz('C',7,250);
+buzz('B',6,500);
+buzz('G',6,125);
+buzz('G',6,125);
+buzz('A',6,250);
+buzz('G',6,250);
+buzz('D',7,250);
+buzz('C',7,500);
+buzz('G',6,125);
+buzz('G',6,125);
+buzz('G',7,250);
+buzz('E',7,250);
+buzz('C',7,250);
+buzz('B',6,250);
+buzz('A',6,250);
+buzz('F',7,125);
+buzz('F',7,125);
+buzz('E',7,250);
+buzz('C',7,250);
+buzz('D',7,250);
+buzz('C',7,500);         
+
+  // buzz(G6, 125);
+  // buzz(G6, 125);
+  // buzz(A6, 250);
+  // buzz(G6, 250);
+  // buzz(C7, 250);
+  // buzz(B6, 500);
+  // buzz(G6, 125);
+  // buzz(G6, 125);
+  // buzz(A6, 250);
+  // buzz(G6, 250);
+  // buzz(D7, 250);
+  // buzz(C7, 500);
+  // buzz(G6, 125);
+  // buzz(G6, 125);
+  // buzz(G7, 250);
+  // buzz(E7, 250);
+  // buzz(C7, 250);
+  // buzz(B6, 250);
+  // buzz(A6, 250);
+  // buzz(F7, 125);
+  // buzz(F7, 125);
+  // buzz(E7, 250);
+  // buzz(C7, 250);
+  // buzz(D7, 250);
+  // buzz(C7, 500);
 }
 
+void buzz(char key, int octave , long length) {
+  long frequency = getFrequency(key, octave);
 
-void buzz(long frequency, long length) {
   long delayValue = WDPI / frequency / 2; // calculate the delay value between transitions
   //// 1 second's worth of microseconds, divided by the frequency, then split in half since
   //// there are two phases to each cycle
