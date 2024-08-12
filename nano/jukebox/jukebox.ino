5290872cd5099c97b9f352b1b86a1d3a3d344aed
isaac <jhhong@navercorp.com>
2024-08-13 00:47:04 +0900

    add 12 Scale with sharp flat

--- nano/jukebox/jukebox.ino
+++ nano/jukebox/jukebox.ino
@@ -1,16 +1,17 @@
-#define MAX_KEYS 10
+#define MAX_KEYS 12
 int currentSize = 0;
 
 struct KeyFrequencyMap {
-  char key;
+  char key[3];  // 두 개의 문자와 null 종결자 '\0'을 위한 공간
   float frequency;
 };
 
 KeyFrequencyMap keyFrequencyMap[MAX_KEYS];
 
-void addKeyValue(char key, float value) {
+void addKeyValue(const char* key, float value) {
   if (currentSize < MAX_KEYS) {
-    keyFrequencyMap[currentSize].key = key;
+    strncpy(keyFrequencyMap[currentSize].key, key, 2); // 두 문자를 복사
+    keyFrequencyMap[currentSize].key[2] = '\0'; // null 종결자 추가
     keyFrequencyMap[currentSize].frequency = value;
     currentSize++;
   }
@@ -20,25 +21,30 @@ long WDPI = 1500000;
 int piezo = 8;
 
 void setup() {
-  addKeyValue('C', 32.7032);
-  addKeyValue('D', 36.7081);
-  addKeyValue('E', 41.2034);
-  addKeyValue('F', 43.6535);
-  addKeyValue('G', 48.9994);
-  addKeyValue('A', 55.0000);
-  addKeyValue('B', 61.7354);
-
+  addKeyValue("C", 32.7032);
+  addKeyValue("C#", 34.6478);
+  addKeyValue("D", 36.7081);
+  addKeyValue("D#", 38.8909);
+  addKeyValue("E", 41.2034);
+  addKeyValue("F", 43.6535);
+  addKeyValue("F#", 46.2493);
+  addKeyValue("G", 48.9994);
+  addKeyValue("G#", 51.9130);
+  addKeyValue("A", 55.0000);
+  addKeyValue("A#", 58.2705);
+  addKeyValue("B", 61.7354);
+  
   pinMode(piezo, OUTPUT);
 
-  HAPPY_BIRTHDAY_TO_YOU();
+  scale();
+  // HAPPY_BIRTHDAY_TO_YOU();
 }
 
-//C, 4
-float getFrequency(char key, int octave){
+float getFrequency(const char* key, int octave){
   float frequency = 0;
 
   for(int i = 0; i < MAX_KEYS; i++) {
-    if(keyFrequencyMap[i].key == key){
+    if(strcmp(keyFrequencyMap[i].key, key) == 0){
       frequency =  keyFrequencyMap[i].frequency;
       break;
     }
@@ -56,70 +62,56 @@ void loop() {
 
 }
 
-//happy birthday to u
-void HAPPY_BIRTHDAY_TO_YOU() {
-buzz('G',6,125);
-buzz('G',6,125);
-buzz('A',6,250);
-buzz('G',6,250);
-buzz('C',7,250);
-buzz('B',6,500);
-buzz('G',6,125);
-buzz('G',6,125);
-buzz('A',6,250);
-buzz('G',6,250);
-buzz('D',7,250);
-buzz('C',7,500);
-buzz('G',6,125);
-buzz('G',6,125);
-buzz('G',7,250);
-buzz('E',7,250);
-buzz('C',7,250);
-buzz('B',6,250);
-buzz('A',6,250);
-buzz('F',7,125);
-buzz('F',7,125);
-buzz('E',7,250);
-buzz('C',7,250);
-buzz('D',7,250);
-buzz('C',7,500);         
-
-  // buzz(G6, 125);
-  // buzz(G6, 125);
-  // buzz(A6, 250);
-  // buzz(G6, 250);
-  // buzz(C7, 250);
-  // buzz(B6, 500);
-  // buzz(G6, 125);
-  // buzz(G6, 125);
-  // buzz(A6, 250);
-  // buzz(G6, 250);
-  // buzz(D7, 250);
-  // buzz(C7, 500);
-  // buzz(G6, 125);
-  // buzz(G6, 125);
-  // buzz(G7, 250);
-  // buzz(E7, 250);
-  // buzz(C7, 250);
-  // buzz(B6, 250);
-  // buzz(A6, 250);
-  // buzz(F7, 125);
-  // buzz(F7, 125);
-  // buzz(E7, 250);
-  // buzz(C7, 250);
-  // buzz(D7, 250);
-  // buzz(C7, 500);
+void scale(){
+  buzz("C", 6,250);
+  buzz("C#",6,250);
+  buzz("D", 6,250);
+  buzz("D#",6,250);
+  buzz("E", 6,250);
+  buzz("F", 6,250);
+  buzz("F#",6,250);
+  buzz("G", 6,250);
+  buzz("G#",6,250);
+  buzz("A", 6,250);
+  buzz("A#",6,250);
+  buzz("B", 6,250);
+  buzz("C", 7,500);  
 }
 
-void buzz(char key, int octave , long length) {
+//happy birthday to u
+void HAPPY_BIRTHDAY_TO_YOU() {
+buzz("G",6,125);
+buzz("G",6,125);
+buzz("A",6,250);
+buzz("G",6,250);
+buzz("C",7,250);
+buzz("B",6,500);
+buzz("G",6,125);
+buzz("G",6,125);
+buzz("A",6,250);
+buzz("G",6,250);
+buzz("D",7,250);
+buzz("C",7,500);
+buzz("G",6,125);
+buzz("G",6,125);
+buzz("G",7,250);
+buzz("E",7,250);
+buzz("C",7,250);
+buzz("B",6,250);
+buzz("A",6,250);
+buzz("F",7,125);
+buzz("F",7,125);
+buzz("E",7,250);
+buzz("C",7,250);
+buzz("D",7,250);
+buzz("C",7,500);         
+}
+
+void buzz(const char* key, int octave , long length) {
   float frequency = getFrequency(key, octave);
 
   long delayValue = WDPI / frequency / 2; // calculate the delay value between transitions
-  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
-  //// there are two phases to each cycle
   long numCycles = frequency * length / 1300; // calculate the number of cycles for proper timing
-  //// multiply frequency, which is really cycles per second, by the number of seconds to
-  //// get the total number of cycles to produce
   for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
     digitalWrite(piezo, HIGH); // write the buzzer pin high to push out the diaphram
     delayMicroseconds(delayValue); // wait for the calculated delay value
