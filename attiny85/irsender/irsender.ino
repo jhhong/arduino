37e29301b8a07916f6508f4e46694802049cd117
isaac <jhhong@navercorp.com>
2024-09-07 01:15:55 +0900

    테스트 환경 코멘트

--- attiny85/irsender/irsender.ino
+++ attiny85/irsender/irsender.ino
@@ -1,7 +1,14 @@
 #include <tiny_IRremote.h>
 #include <avr/sleep.h>
 
-// IMPORTANT: IRsend only works from PB4 ("pin 4" according to Arduino)
+// IRsend 는 반드시 PB4 에만 연결 해야 정상 동작함
+// PB4 는 별도로 pinMode 해줄 필요없음
+// CR2032 정도의 전압에 연결시 적외선 LED와 피드백용 LED(PB0) 에 저항을 연결하지 않아도 정상동작
+// PC의 USB로 전원 연결시 적외선 LED와 피드백용 LED(PB0) 에 저항(테스트:330옴)을 반드시 연결해야 정상 동작함.
+// 적외선 송신 LED는 수신 LED의 방향에 일직선으로 일치해야 수신됨. 30-40cm의 가까운 거리라도 방향 불일치시 수신 안됨.
+// 위로 송신하거나 손에 가리면 전송이 안됨(최대 길이 테스트 필요)
+// 신호 증폭가능한 방법 확인 필요
+
 int LED_PIN = PB0;
 int SWITCH_PIN = PB3;
 
