0b94c68781d52210fd1fbd6cf18f2efd2fb6261a
isaac <jhhong@navercorp.com>
2024-11-04 00:46:08 +0900

    servo example

---
+++ nano/motor/servo/servo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int pos = 0;
+
+void setup() {
+  myservo.attach(8);
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) {
+    
+    myservo.write(pos);
+    delay(10);
+  }
+  
+  for (pos = 180; pos >= 0; pos -= 1) {
+    myservo.write(pos);
+    delay(10);
+  }
+}
